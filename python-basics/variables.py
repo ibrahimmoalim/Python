3a2ce045df9625976dadf28c17ee9c7841f353c0
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-28 21:51:13 +0300

    add variables.py

---
+++ python-basics/variables.py
@@ -0,0 +1,23 @@
+# Variables = A container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains
+
+# Strings
+my_var = 'bro'
+food = 'pasta'
+email = 'yahya123@fake.com'
+
+print(f'hi {my_var}') # hi bro
+
+print(f'bro you like {food}') # bro you like pasta
+
+print(f'your email is: {email}') # your email is: yahya123@fake.com
+
+# Integers
+var_number = 123
+age = 26
+num_of_students = 40
+
+print(var_number) # 123
+print(f'you are {age} years old') # you are 26 years old
+print(f'you have {num_of_students} students') # you have 40 students
+
