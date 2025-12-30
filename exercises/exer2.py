fe8689760372f907ec7b04fcb1c928d52991b351
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-30 17:28:09 +0300

    add exercise 2

---
+++ exercises/exer2.py
@@ -0,0 +1,14 @@
+# validate user input exercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 2. username must not contain digits
+
+username = input('Enter your username: ')
+
+# only a-z, A-Z, "_", and "." are allowed
+allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.'
+
+if len(username) <= 12 and all(char in allowed for char in username):
+  print(f'{username} is valid.')
+else:
+  print(f'{username} is not valid, Please make sure username 12 characters or fewer, contains only letters, "_" or ".", contains NO spaces or numbers.')
