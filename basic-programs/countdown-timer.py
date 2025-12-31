372a389dcb0f3f5ca94cabdd34c6a03071eed704
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2025-12-31 08:10:31 +0300

    add timer

---
+++ basic-programs/countdown-timer.py
@@ -0,0 +1,18 @@
+# built-in time module
+import time
+
+timer = int(input('Enter time in seconds: '))
+
+# (start) from input value, (stop) before -1 ,(step) "-1" does reverse range
+for x in range(timer, -1, -1):
+  # seconds is equal to remainder of x / 60
+  seconds = x % 60
+  # // is floor division (no decimals).
+  minutes = (x // 60) % 60
+  hours = (x // 3600)
+  # :02 "2" digits will be always shown, if the number is one digit, it'll put 1 "0" infron of it.
+  print(f'{hours:02}:{minutes:02}:{seconds:02}')
+  # pauses for 1 second before the next iteration
+  time.sleep(1)
+
+print("Time's UP!")
