d59fab3aa67882ce0903cd0338cd99b25f870813
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-01-07 08:31:34 +0300

    remake and add more features

--- basic-programs/hangman.py
+++ basic-programs/hangman.py
@@ -1,59 +1,96 @@
 # Hangman game
 import random
 
-countries = ('Krygyzstan', 'Eswatini', 'Argentina', 'Russia', 'Slovenia', 'Malawi')
-animals = ('Goat', 'Sloth', 'Hedgehog', 'Jaguar', 'Raccoon', 'Otter', 'Eagle', 'Dolphin')
-fruits = ('Banana', 'Coconut', 'Watermelon', 'Guava')
+def main():
 
+  countries = ('Kyrgyzstan', 'Eswatini', 'Argentina', 'Russia', 'Slovenia', 'Malawi', 'Taiwan', 'Kuwait', 'Senegal', 'Brazil', 'Somalia', 'Indonesia', 'Afghanistan', 'Vietnam', 'Ireland')
+  animals = ('Goat', 'Sloth', 'Hedgehog', 'Jaguar', 'Raccoon', 'Otter', 'Eagle', 'Dolphin', 'Camel', 'Elephant', 'Donkey', 'Lion', 'Cat', 'Dog', 'Pigeon', 'Penguin', 'Falcon', 'Horse')
+  fruits = ('Banana', 'Coconut', 'Watermelon', 'Guava', 'Apple', 'Grape', 'Strawberry', 'Blueberry', 'Cherry', 'Pineapple', 'Mango', 'Kiwi', 'Date', 'Raisin')
+  
+  try:
+    while True:
+      category = input('Pick one of these categories to start (A. Countries, B. Animals, C. Fruits) or q to quit: ').lower()
 
-try:
+      if category == 'q':
+        print('Bye!')
+        exit()
 
-  while True:
-    category = input('Pick one of these categories to start (A. Countries, B. Animals, C. Fruits) or q to quit: ').lower()
+      elif category in ('a', 'countries'):
+        play_game('Country', countries)
 
-    if category == 'q':
-      print('Bye!')
-      exit()
+      elif category in ('b', 'animals'):
+        play_game('Animal', animals)
+      
+      elif category in ('c', 'fruits'):
+        play_game('Fruit', fruits)
 
-    if category == 'a' or category == 'countries':
-      country = random.choice(countries).lower()
-      # for x in country:
-      #   word.append('_')
-      word = ['_' for _ in country]
-      tries = 5
-      while True:
+      else:
+        print('Invalid input!')
+  except KeyboardInterrupt:
+    print('\nCtrl+C Detected. Exiting Gracefully!')
+    exit()
 
-        print(f'Country: ', *word)
-        print(f'You have {tries} tries')
-        guess = input('Guess a letter or the whole word in one try!: ').lower()
-        if guess == country:
-          print(f'Correct. Country is {country}')
+def play_game(label, words):
+  try:
+
+    tries = 6
+    secret = random.choice(words).lower()
+    hidden = ['_' for _ in secret]
+    won = False
+
+    while tries > 0:
+
+        print(f'{label}: ', *hidden, f'tries: {tries}')
+        guess = input('Guess a letter or the whole word: ').lower()
+
+        if not guess.isalpha():
+          print('Letters only!')
+          continue
+
+        if guess == secret:
+          print('*****************')
+          print(f'Correct. {label} is {secret.capitalize()}.')
+          print('*****************')
+          won = True
           break
 
-        elif guess in country:
-          for i in range(len(country)):
-            if country[i] == guess:
-              word[i] = guess
-          if word == list(country):
-            print(f'Welldone. Country is {country}')
-            break
+        elif len(guess) != 1:
+          print('Invalid. Guess one letter at a time!')
+          continue
 
         else:
-          print('Incorrect. Try again!')
-          tries -= 1
-          print(f'You have {tries} tries left')
+          if guess in secret:
+            for i in range(len(secret)):
+              if secret[i] == guess:
+                hidden[i] = guess
 
-      # if (Play_again:= input('Play again? (y/n): ').lower()) != 'y':
-      #   exit()
+            if hidden == list(secret):
+              print('*****************')
+              print(f'Well done! {label} is {secret.capitalize()}.')
+              print('*****************')
+              won = True
+              break
+          
+          else:
+            tries -= 1
+          if tries == 0:
+            break
+          else:
+            print('Incorrect. Try again!')
+            if tries == 1:
+              print(f'You have {tries} try left')
+            else:
+              print(f'You have {tries} tries left')
 
-    elif category == 'b' or category == 'animals':
-      print('b')
+    if not won:
+      print('*****************')
+      print(f'You lose! The {label} was {secret.capitalize()}.')
+      print('*****************')
 
-    elif category == 'c' or category == 'fruits':
-      print('c')
+  except KeyboardInterrupt:
+    print('\nCtrl+C Detected. Exiting Gracefully!')
+    exit()
 
-    else:
-      print('Invalid input!')
 
-except KeyboardInterrupt:
-  print('\nCtrl+C Detected. Exiting Gracefully!')
+if __name__ == "__main__":
+  main()
