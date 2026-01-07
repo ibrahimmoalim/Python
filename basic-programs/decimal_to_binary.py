9d3b78f89728d841e0783bd883646b661e7d55c0
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-01-07 12:07:34 +0300

    add decimal to binary conversion program

---
+++ basic-programs/decimal_to_binary.py
@@ -0,0 +1,95 @@
+# decimal <=> binary
+
+def main():
+  try:
+    while True:
+      try:
+        decimal = input('Enter an IP address to trun into Binary (q to quit): ')
+
+        if decimal.lower() == 'q':
+          print('Bye!')
+          exit()
+        
+        else:
+          decimal_to_binary(decimal)
+      except ValueError:
+        print('Invalid input!')
+      except IndexError:
+        print('Invalid input!')
+  except KeyboardInterrupt:
+    print('\nCtrl+C Detected. Exiting Gracefully!')
+    exit()
+
+def decimal_to_binary(decimal):
+  chart = 128,64,32,16,8,4,2,1
+  octets = decimal.split('.')
+
+  if len(octets) > 4:
+    print('Invalid input, octets should be only 4.')
+    return
+  
+  first_octet = int(octets[0])
+  second_octet = int(octets[1])
+  third_octet = int(octets[2])
+  fourth_octet = int(octets[3])
+  
+  for octet in octets:
+    if len(octet) not in (1, 2, 3):
+      print('Invalid input, digits in each octet must not be lower than 1, nor higher than 3!')
+      return
+
+  first_8bits = []
+  second_8bits = []
+  third_8bits = []
+  fourth_8bits = []
+
+  temp = 0
+
+  for x in chart:
+    if first_octet - x >= 0:
+      temp = first_octet - x
+      first_8bits.append(1)
+      first_octet = temp
+    else:
+      first_8bits.append(0)
+  
+  for x in chart:
+    if second_octet - x >= 0:
+      temp = second_octet - x
+      second_8bits.append(1)
+      second_octet = temp
+    else:
+      second_8bits.append(0)
+  
+  for x in chart:
+    if third_octet - x >= 0:
+      temp = third_octet - x
+      third_8bits.append(1)
+      third_octet = temp
+    else:
+      third_8bits.append(0)
+  
+  for x in chart:
+    if fourth_octet - x >= 0:
+      temp = fourth_octet - x
+      fourth_8bits.append(1)
+      fourth_octet = temp
+    else:
+      fourth_8bits.append(0)
+  
+  first_str = ''.join(map(str, first_8bits))
+  second_str = ''.join(map(str, second_8bits))
+  third_str = ''.join(map(str, third_8bits))
+  fourth_str = ''.join(map(str, fourth_8bits))
+
+  print('*************************')
+  print(f'Decimal: {decimal}')
+  print('Binary:', end=' ')
+  print(first_str, second_str, third_str, fourth_str, sep='.')
+  print('*************************')
+
+def binary_to_decimal(binary):
+  pass
+
+if __name__ == "__main__":
+  main()
