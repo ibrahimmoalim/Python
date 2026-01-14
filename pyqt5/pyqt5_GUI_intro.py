4cb52cce151d027614c87c7a9689a22071372466
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-01-14 07:59:15 +0300

    add text and text styling

--- pyqt5/pyqt5_GUI_intro.py
+++ pyqt5/pyqt5_GUI_intro.py
@@ -4,9 +4,12 @@
 # such as command-line arguments and exit handling. It is part of Python’s standard library.
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-# work with window icons
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+# work with window icons and fonts
+from PyQt5.QtGui import QIcon, QFont
+# 'Qt' class is used for alignments like text centring
+from PyQt5.QtCore import Qt
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -20,6 +23,37 @@ class MainWindow(QMainWindow):
       self.setGeometry(700, 300, 700, 550)
       # set icon image (make the path absolute if relative path doesn't work)
       self.setWindowIcon(QIcon("/home/ibrahim/python/pyqt5/face.png"))
+
+      # add text to the window, like html paragraph
+      label = QLabel('Hi lad!', self)
+      # change position of text (x, y)
+      # label.move(0, 0)
+      # or change position and text-size once with .setGeometry(x, y, width, height)
+      label.setGeometry(0, 0, 700, 555)
+      # change font and font-size
+      label.setFont(QFont('Arial', 24))
+      # use styles similar to css styles
+      label.setStyleSheet("color: red;" # you could use rgb(255, 0, 0)
+                          # rgba for transparency, a = alpha (0-255) or (0-1)
+                          "background-color: rgba(0, 0, 255, 0.2);" # you could just use 'lightblue' without ''
+                          "font-weight: bold;"
+                          "font-style: italic;"
+                          "text-decoration: underline;")
+        # Alignments/positioning
+      # label.setAlignment(Qt.AlignTop) # align to the top of it's sized block (you can tell it's block (amount of space it takes) by giving it background-color)
+      # label.setAlignment(Qt.AlignBottom) # align to the bottom of it's sized block
+      label.setAlignment(Qt.AlignVCenter) # align to the center(V = Vertical) of it's sized block (default)
+
+      # label.setAlignment(Qt.AlignRight) # align to the right of it's sized block
+      # label.setAlignment(Qt.AlignLeft) # align to the left of it's sized block
+      label.setAlignment(Qt.AlignHCenter) # align to the center(H = Horizontal) of it's sized block (default)
+
+      # combine both horizontal and vertical positioning with "|" ( "|" allows us to combine flags)
+      label.setAlignment(Qt.AlignHCenter | Qt.AlignTop) # top vertically and horizontally centered
+      # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) # fully centered
+      # shortcut for fully centring text
+      label.setAlignment(Qt.AlignCenter) # fully centered
+      
 
 
 def main():
